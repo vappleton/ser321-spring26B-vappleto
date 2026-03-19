36db1b29dfff038e754a2f2c7fee4cbe9d0730ac
vappleton <139707137+vappleton@users.noreply.github.com>
2026-03-19 16:20:39 -0700

    Add runFrac task in build.gradle and edit Fraction.java to accept 2 arguments

---
+++ Assignment1/JavaGradle/src/main/java/Fraction.java
@@ -0,0 +1,69 @@
+import java.io.*;
+/**
+ * Purpose: demonstrate simple Java Fraction class with command line,
+ * jdb debugging, and Ant build file.
+ *
+ * Ser321 Foundations of Distributed Applications
+ * see http://pooh.poly.asu.edu/Ser321
+ * @author Tim Lindquist Tim.Lindquist@asu.edu
+ *         Software Engineering, CIDSE, IAFSE, ASU Poly
+ * @version January 2020
+ */
+public class Fraction {
+
+   private int numerator, denominator;
+
+   public Fraction(){
+      numerator = denominator = 0;
+   }
+
+   public void print() {
+    System.out.print(numerator + "/" + denominator );
+   }
+
+   public void setNumerator (int n ){
+      numerator = n;
+   }
+
+   public void setDenominator (int d) {
+      denominator = d;
+   }
+
+   public int getDenominator() {
+      return denominator;
+   }
+
+   public int getNumerator() {
+      return numerator;
+   }
+
+   public static void main (String args[]) {
+      try {
+          int num =  1;
+          int denom = 1;
+
+          if (args.length >= 1) {
+              num = Integer.parseInt(args[0]);
+          }
+          if (args.length >=2 ) {
+              denom = Integer.parseInt(args[1]);
+          }
+         // create a new instance
+         // Fraction *frac = [[Fraction alloc] init];
+         Fraction frac = new Fraction();
+
+         // set the values
+         frac.setNumerator(num);
+         frac.setDenominator(denom);
+
+         // print it
+         System.out.print("The fraction is: ");
+         frac.print();
+         System.out.println("");
+
+      }catch(Exception e) {
+         e.printStackTrace();
+      }
+   }
+}
+
