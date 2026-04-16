04efadb14b0684000524c87d98da3b21cf560fd8
vappleton <139707137+vappleton@users.noreply.github.com>
2026-04-15 22:25:55 -0700

    Add additional tests

--- Assignment4/activity2-starter/src/test/java/auction/ProtocolTest.java
+++ Assignment4/activity2-starter/src/test/java/auction/ProtocolTest.java
@@ -150,6 +150,108 @@ public class ProtocolTest {
     // ADD YOUR TESTS BELOW (at least 3 more tests required)
     // ====================================================================
 
+    @Test
+    @Order(5)
+    public void testJoinRequest() throws IOException {
+        Response.parseDelimitedFrom(in);
+
+       sendRegister("JoinTestPlayer");
+
+       sendJoin();
+
+       Response response = Response.parseDelimitedFrom(in);
+       assertNotNull(response);
+       assertEquals(Response.ResponseType.GAME_JOINED, response.getType());
+       assertTrue(response.getOk());
+
+       assertTrue(response.hasPlayerStatus(), "Should include player status");
+       assertEquals(150, response.getPlayerStatus().getGoldRemaining(), "the player should start with 150 gold");
+       assertTrue(response.hasNextItem(), "should have the first auction item");
+
+    }
+
+    @Test
+    @Order(6)
+    public void testValidBidRequest() throws IOException {
+        Response.parseDelimitedFrom(in);
+        sendRegister("ValidBidTestPlayer");
+        sendJoin();
+        //read game_joined repsonse
+        Response joinResponse = Response.parseDelimitedFrom(in);
+        assertNotNull(joinResponse);
+        assertEquals(Response.ResponseType.GAME_JOINED, joinResponse.getType());
+
+        int itemId = joinResponse.getNextItem().getId();
+        int reserve = joinResponse.getNextItem().getReservePrice();
+
+        //send a valid bit at reserve price
+        Request bidRequest = Request.newBuilder()
+                .setType(Request.RequestType.BID)
+                .setItemId(itemId)
+                .setBidAmount(reserve)
+                .build();
+        bidRequest.writeDelimitedTo(out);
+        Response response = Response.parseDelimitedFrom(in);
+
+        assertNotNull(response);
+        assertEquals(Response.ResponseType.BID_RESULT, response.getType(), "a valid bid should return BID_RESULT");
+
+        assertTrue(response.getOk(), "a valid bid should succedd");
+
+        assertTrue(response.hasResult(), "Response should includ ethe auction results");
+        assertTrue(response.hasPlayerStatus(), "Response should includ player status");
+
+
+    }
+    @Test
+    @Order(7)
+    public void testInvalidBidRequest() throws IOException {
+        Response.parseDelimitedFrom(in);
+        sendRegister("InvalidBidTestPlayer");
+        sendJoin();
+        Response joinResponse = Response.parseDelimitedFrom(in);
+        int itemId = joinResponse.getNextItem().getId();
+
+        //sending an invalid bid
+        Request bidRequest = Request.newBuilder()
+                .setType(Request.RequestType.BID)
+                .setItemId(itemId)
+                .setBidAmount(1) //testing with 1 first to see if its below reserve
+                .build();
+        bidRequest.writeDelimitedTo(out);
+        Response response = Response.parseDelimitedFrom(in);
+
+        assertEquals(Response.ResponseType.ERROR, response.getType(), "an invalid bid should return ERROR");
+        assertFalse(response.getOk());
+        assertTrue(response.getMessage().contains("reserve"), "Error should mention reserve price");
+
+    }
+    @Test
+    @Order(8)
+    public void testSkipBidRequest() throws IOException {
+        Response.parseDelimitedFrom(in); //welcome
+
+        sendRegister("SkipBidTester");
+        sendJoin();
+        Response joinResponse = Response.parseDelimitedFrom(in);
+        int itemId = joinResponse.getNextItem().getId();
+
+        //send skip bid
+        Request bidRequest = Request.newBuilder()
+                .setType(Request.RequestType.BID)
+                .setItemId(itemId)
+                .setBidAmount(-1)
+                .build();
+
+        bidRequest.writeDelimitedTo(out);
+        Response response = Response.parseDelimitedFrom(in);
+
+        assertEquals(Response.ResponseType.BID_RESULT, response.getType(), "skip should still output BID_RESULT");
+        assertTrue(response.getOk());
+        assertTrue(response.hasResult(),"Should include auction results");
+
+    }
+
 
 
     // Helper methods if you want to use them
