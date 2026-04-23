a652c90082c3e80dae39328750f7853388ca4cb8
vappleton <139707137+vappleton@users.noreply.github.com>
2026-04-22 22:57:56 -0700

    Fix  coordination logic

--- Assignment5/activity1/src/main/java/Worker.java
+++ Assignment5/activity1/src/main/java/Worker.java
@@ -22,7 +22,7 @@ public class Worker {
     public static void main(String[] args) throws IOException {
 
         if (args.length < 3) {
-            System.out.println("Please enter command in this format: gardle runWorker --args=<name> <host> <port>");
+            System.out.println("Please enter command in this format: gradle runWorker --args=<name> <host> <port>");
             return;
         }
 
@@ -50,20 +50,35 @@ public class Worker {
             while (true) {
                 String message = in.readLine();
 
-                if (message == null) {
-                    System.out.println("Leader disconnected.");
+                if (message == null || message.equals("QUIT")) {
+                    System.out.println("Leader disconnected.Shutting down");
                     break;
                 }
-                //task was received
+                //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~TASK RECEIVED~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
                 if (message.startsWith("TASK")) {
                     String task = message.substring(5); //rebuild "TASK"
                     System.out.println("Task received: " + task);
 
-                    int userResult;
+                    int userResult = 0;
 
                     while (true) {
 
                         System.out.println("> Enter your result: ");
+
+                        if (in.ready()) { // check if Leader sent something new while waiting
+                            String newMessage = in.readLine();
+
+                            if (newMessage == null) {
+                                System.out.println("Leader disconnected. Shutting down.");
+                                return;
+                            }
+                            message = newMessage;
+                            break; // exit input loop and process the new message
+                        }
+                        if (!scanner.hasNextLine()) {
+                            System.out.println("Input closed.");
+                            return;
+                        }
                         String input = scanner.nextLine();
 
                         try {
@@ -73,14 +88,44 @@ public class Worker {
                             System.out.println("Please enter a number!");
                         }
 
-                    }
-                    lastVote = userResult;
+                    } // if interrupted, process the new message immediately
+                    if (!message.startsWith("TASK")) {
 
+                        if (message.equals("QUIT")) {
+                            System.out.println("Leader disconnected. Shutting down.");
+                            break;
+                        } else if (message.startsWith("CONSENSUS")) { //consesus received
+
+                            String[] parts = message.split(" ");
+
+                            int consensus = Integer.parseInt(parts[1]);
+
+                            String ratio = parts[2];
+
+                            System.out.println("Consensus announced: " + consensus + " (" + ratio + " workers agreed)");
+
+                            if (consensus == lastVote) {
+                                System.out.println("You voted with the majority!\n");
+                            } else {
+                                System.out.println("Your answer differed from the consensus.\n");
+                            }
+
+                            System.out.println("Waiting for next task...\n");
+                            continue;
+                        }
+                        continue;
+                    }
+
+                    //send the result
                     out.println("RESULT " + userResult);
+                    lastVote = userResult;
 
                     System.out.println("Result submitted to leader.\n");
 
-                } else if (message.startsWith("CONSENSUS")){ //consesus received
+                }
+
+                //~~~~~~~~~~~~~~~~~~~~~~~~~~~CONSENSUS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+                else if (message.startsWith("CONSENSUS")) { //consesus received
 
                     String[] parts = message.split(" ");
 
@@ -99,19 +144,13 @@ public class Worker {
                     System.out.println("Waiting for next task...\n");
                 }
 
-                // ERROR CONSENSUS
+                // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ERROR CONSENSUS~~~~~~~~~~~~~~~~~~~~~~~//
                 else if (message.startsWith("ERR-CONS")) {
                     System.out.println("Consensus failed: " + message);
                     System.out.println("Waiting for next task...\n");
                 }
-
-                // QUIT
-                else if (message.equals("QUIT")) {
-                    System.out.println("Shutting down.");
-                    break;
-                }
-
             }
+
             socket.close();
         } catch (IOException e) {
             System.out.println("Connection lost. Exiting!");
