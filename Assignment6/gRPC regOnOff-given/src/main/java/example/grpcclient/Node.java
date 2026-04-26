20702afee30e73396ef61b9526ba97cd71efeef8
vappleton <139707137+vappleton@users.noreply.github.com>
2026-04-25 21:47:56 -0700

    Add Converter service to the list of services

--- Assignment6/gRPC regOnOff-given/src/main/java/example/grpcclient/Node.java
+++ Assignment6/gRPC regOnOff-given/src/main/java/example/grpcclient/Node.java
@@ -37,7 +37,9 @@ public class Node {
     server = ServerBuilder.forPort(port)
         .addService(new EchoImpl())
         .addService(new JokeImpl())
-        .addService(new RegistryAnswerImpl(services)).build().start();
+        .addService(new RegistryAnswerImpl(services))
+        .addService(new ConverterImpl())
+        .build().start();
 
     for (var service : server.getServices()) {
       // returns the services that are available from this node
